cc67f9a42d56bd1982b9e6e8143ca4abf523fbdd
pazgu <pazgueta@gmail.com>
2026-06-15 17:32:12 +0300

    add implementation of lucky number checker with promise handling

---
+++ vanilla-js/promises/ex1.js
@@ -0,0 +1,21 @@
+//Create a promise that simulates checking if a number is lucky.
+
+function checkLuckyNumber(num) {
+  // TODO: Create and return a promise that:
+  // 1. Waits 800ms (use setTimeout)
+  // 2. Resolves with "Lucky!" if number is divisible by 7
+  // 3. Resolves with "Not lucky" for other positive numbers
+  // 4. Rejects with Error("Invalid number") if number is negative or zero
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (num <= 0) {
+        reject(new Error("Invalid number"));
+      } else if (num % 7 === 0) {
+        resolve("Lucky!");
+      } else {
+        resolve("Not lucky");
+      }
+    }, 800);
+  });
+}
