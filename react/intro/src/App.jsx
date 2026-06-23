dd3e01cb068147cba11f8322d41807b0dbf03f6d
pazgu <pazgueta@gmail.com>
2026-06-23 15:29:42 +0300

    refactor: reorganize App component structure and enhance temperature class logic

--- react/intro/src/App.jsx
+++ react/intro/src/App.jsx
@@ -15,11 +15,29 @@ function App() {
     );
   };
 
+  const getClassName = (temperature) => {
+    if (temperature)
+      if (temperature < 15) return "freezing";
+      else if (temperature > 30) return "hell-scape";
+    return "fair";
+  };
+
+  const currentTemp = 25;
+
   return (
-    <div className="ex-space">
-      <h4 className="ex-title">Exercise 1</h4>
-      <div className="exercise" id="ex-1">
-        {companies.map((c) => showCompany(c.name, c.revenue))}
+    <div className="app-container">
+      <div className="ex-space">
+        <h4 className="ex-title">Exercise 1</h4>
+        <div className="exercise" id="ex-1">
+          {companies.map((c) => showCompany(c.name, c.revenue))}
+        </div>
+      </div>
+
+      <div className="ex-space">
+        <h4 className="ex-title">Exercise 2</h4>
+        <div className="exercise" id="ex-2">
+          <div className={getClassName(currentTemp)} id="weatherBox"></div>
+        </div>
       </div>
     </div>
   );
